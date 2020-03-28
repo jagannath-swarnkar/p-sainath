6d7a71a11cd583679b407ea1b5642dba37f80217
Abhishek Gupta (via Travis CI) <abhishekgupta.iitd@gmail.com>
2020-03-28 20:27:09 +0000

    Deploy navgurukul/p-sainath to github.com/navgurukul/p-sainath.git:gh-pages

--- src/assets/js/iframe.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  var iWidth = $('#textContent').width();
-  var iHeight = iWidth * 9 / 16;
-  $("iframe#leftTextContent").width(iWidth);
-  $("iframe#leftTextContent").height(iHeight);
-});
